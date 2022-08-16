cdd564d8ecdfc95694de77faf1f368b4789da4c0
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-08-16 21:19:55 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+
+<script>
+	import { TextBox,TextBlock,ToggleSwitch,ListItem,Button,AutoSuggestBox  } from "fluent-svelte";
+</script>
+
+
+<style>
+    @import url("https://unpkg.com/fluent-svelte/theme.css");
+
+    /* Some base styles to get things looking right. */
+    :global(body) {
+        background-color: var(--fds-solid-background-base);
+        color: var(--fds-text-primary);
+    }
+	div.relative {
+           position: fixed;
+           left: 36%;
+
+		width:300px;  align-items: center;
+}
+		div.new {
+    text-align: center;
+}
+			div.Text {
+				    text-align: center;
+				font-family: "Segoe UI";
+				font-size:128px;
+				font-weight: 600;
+}
+	
+</style>
+
+<div class="relative">
+<AutoSuggestBox placeholder="Search"/>
+</div>
+<div class="new">
+	<Button variant="accent" >Subscribe</Button>
+</div>
+<div class="Text">
+	    <p>The<br>OA</p>
+</div>
